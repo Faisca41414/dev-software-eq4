547f66a1fb95627f0c8dc32d073b2696ad96e56a
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-11-29 17:28:59 -0300

    warn if the project is executed incorrectly

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+import sys
+if ("fastapi" not in  sys.argv[0]): 
+    raise Exception("\n\t🦄🦄🦄🦄🦄🦄🦄🦄\033[1;31m Please run this file with 'fastapi run dev'")
 app = FastAPI()
 
 
