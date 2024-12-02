21fbbbc83e6583fbbd3af45e4b74e3aebdd10e34
AndreFGard@gmail.com <andrefsagh@gmail.com>
2024-12-02 09:54:10 -0300

    add sample message box to the frontend

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,49 +1,78 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './Counter.svelte'
-  import {apiUrl} from './api.js'
-</script>
+    import svelteLogo from './assets/svelte.svg'
+    import viteLogo from '/vite.svg'
+    import Counter from './Counter.svelte'
+    import {apiUrl, addFetchData} from './api.js'
 
-<main>
-  <div>
-    <p>teste {apiUrl}</p>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a> 
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+    let messages=[{username: 'gpt', message:'hello, im gpt'}];
+    let username='Fulano';
+    let message='';
+    let error=''
 
-  <div class="card">
-    <Counter />
-  </div>
+    async function  handleAdd() {
+      try {        
+        messages =  await addFetchData({username:username, message:message});
+        console.log(`messages: ${messages}`)
+        message='';
+      } catch (e: any){
+        error = e.message || e;
+      }
+    }
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+  </script>
+  
+  <main>
+    <div>
+      <h3>Send message:</h3>
+      <input placeholder="Type your message" bind:value={message}/>
+      <button onclick="{handleAdd}">Send</button>
+      <h3>Messages:</h3>
+      <ul>
+        {#each messages as msg}
+          <li>{msg.username}: {msg.message}</li>
+        {/each}
+
+
+      <p>url da api {apiUrl}</p>
+      <a href="https://vite.dev" target="_blank" rel="noreferrer">
+        <img src={viteLogo} class="logo" alt="Vite Logo" />
+      </a> 
+      <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+        <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+      </a>
+    </div>
+    <h1>Vite + Svelte</h1>
+  
+    <div class="card">
+      <Counter />
+    </div>
+  
+    <p>
+      Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    </p>
+  
+    <p class="read-the-docs">
+      Click on the Vite and Svelte logos to learn more
+    </p>
+  </main>
+  
+  <style>
+    .logo {
+      height: 6em;
+      padding: 1.5em;
+      will-change: filter;
+      transition: filter 300ms;
+    }
+    .logo:hover {
+      filter: drop-shadow(0 0 2em #646cffaa);
+    }
+    .logo.svelte:hover {
+      filter: drop-shadow(0 0 2em #ff3e00aa);
+    }
+    .read-the-docs {
+      color: #888;
+    }
+  </style>
+  
---
+++ frontend/src/api.ts
@@ -0,0 +1,19 @@
+export const apiUrl= import.meta.env.VITE_API_URL
+
+interface Message{
+    username: string;
+    message:string;
+}
+
+
+export async function addData(msg: Message){
+    const response = await fetch(`${apiUrl}/addData`, {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(msg),
+    })
+    console.log(response);
+    return response.json()
+}
