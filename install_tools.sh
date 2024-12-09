fabdf505e119417173c31fccf307545790f7d316
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-12-09 08:40:15 -0300

    Create install_tools.sh
    Easier way to replicate the development environment in unix systems

---
+++ install_tools.sh
@@ -0,0 +1,18 @@
+pipurl="https://bootstrap.pypa.io/get-pip.py"
+wget $pipurl
+python3 get-pip.py
+echo '$PATH=$PATH:$HOME/.local/bin' >> $HOME/.bashrc
+export $PATH=$PATH:$HOME/.local/bin
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+alias python=python3
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 23
+cd frontend
+npm install 
+cd ..
