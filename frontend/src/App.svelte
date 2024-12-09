9d98cf76a348a6e327cbd9b5eed8e287b0e9e000
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-12-09 11:00:31 -0300

    add bulma css to App.svelte

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -22,10 +22,16 @@
   }
 </script>
 
-<main>
-  <div>
-    <h3>This is the chat component:</h3>
+<main class="section">
+  <div class="container" id="maincontainer">
+    <h3 class="title  has-text-centered">Chat component</h3>
     <!--  Repare que passamos pro chat a função handleAdd definida aqui que sera chamada quando o botao for apertado-->
     <Chat {handleAdd} bind:messages={messages} bind:message={message} />
   </div>
 </main>
+
+<style>
+  #maincontainer{
+    max-width: 90vh;
+  }
+</style>
