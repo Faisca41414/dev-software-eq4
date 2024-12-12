805c59d5cc0cf5d6af196e0489e765fa97644d91
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-12-12 10:05:34 -0300

    (city validator) add City Database

--- model.py
+++ model.py
@@ -54,8 +54,55 @@ class User():
     
     def getMessageHistory(self) -> List[Message]:
         return [Message(username=item.username, content=item.content) for item in self.message_history]
-    
 
+
+class City(BaseModel:
+    name:str
+    state:str
+    country:str
+    data:dict
+    id: int
+
+class CityValidator:
+    def __init__(self):
+        import json
+        filename="countrie_states_cities.json"
+        fileurl='https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/refs/heads/master/json/countries%2Bstates%2Bcities.json'
+        f = None
+        try:
+            f = open(filename)
+        except:
+            import os
+            os.system(f"curl {fileurl} -o {filename}")
+            f = open(filename)
+        j = json.load(f)
+        countries = {ct["name"]:ct for ct in j}
+        cities ={}
+        city_ids = {}
+        for ct in countries.values():
+            ct['states'] = {st["name"]:st for st in ct['states']}
+            for st in ct["states"].values():
+                st['cities'] = {city['name']:city for city in st['cities']}
+                for city in st['cities'].values():
+                    if city['name'] not in cities:
+                        cities[city['name']] = []
+                    
+                    cities[city['name']].append({'name':city['name'], 'state': st['name'], 'country': ct['name'], 'data':city, 'id':city['id']})
+                    city_ids[city['id']] = city
+        
+        self.countries = countries
+        self.cities = cities
+        self.city_ids = city_ids
+
+    #TODO
+    def city_matches_name(self, name:str) ->List[City]:
+        """"Retorna todas as cidades com o mesmo nome"""
+        return self.cities['Recife']
+        pass
+    
+    #TODO
+    def city_matches_name_state(name:str, state:str) -> List[City]:
+        pass
 
 #exemplo#
 rob = User(username="Robinson", message_history=[])
